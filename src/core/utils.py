b52b94e66600c92f650ee87bd6b164f980362226
raj-open <raj-open@users.noreply.github.com>
2022-10-27 12:15:03 +0200

    main > main: src - added utility fct, binary seq

--- src/core/utils.py
+++ src/core/utils.py
@@ -16,6 +16,7 @@ from src.thirdparty.types import *;
 __all__ = [
     'flatten',
     'get_timestamp_string',
+    'binary_sequences',
 ];
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -34,3 +35,11 @@ def get_timestamp_string() -> str: # pragma: no cover
 
 def flatten(X: list[list[Any]]) -> list[Any]:
     return list(itertools_chain.from_iterable(X));
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# METHODS maths
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def binary_sequences(n: int) -> list[str]:
+    seq = itertools_product(*([[0,1]]*n));
+    return [ ''.join([str(x) for x in c]) for c in seq ];
